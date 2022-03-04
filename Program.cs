84fb76250beb730222c5c81e06c56d4e7f8ab3d5
Thiago <thiagolopespereiralp1@gmail.com>
2022-03-04 18:37:22 -0300

    terminei a licao

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.WriteLine(" ||  || ");
+Console.WriteLine("  \\()//");
+Console.WriteLine("//(__)\\");
+Console.WriteLine("||    ||");
